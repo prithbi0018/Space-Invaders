ce5d675ffeb1733863c935200b6f8768e0b969d2
prithbi0018 <prithbirajpanda@gmail.com>
2024-05-27 14:43:23 +0530

    added sfml windo

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,28 @@
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+    // Create the SFML window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Game Window");
+
+    
+    while (window.isOpen())
+    {
+       
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+       
+        window.clear();
+
+  
+      
+        window.display();
+    }
+
     return 0;
 }
