faaedc7eb76d9144b87152519ca2fc88d70e6c32
prithbi0018 <prithbirajpanda@gmail.com>
2024-06-04 16:42:37 +0530

    implemented the PlayerModel

---
+++ Space-Invaders/Header/PlayerModel.h
@@ -0,0 +1,38 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+
+
+class PlayerView {};
+class Playercontrollor {};
+
+class PlayerModel
+{
+private:
+	const sf::Vector2f initial_player_position = sf::Vector2f(500.0f, 500.0f);
+	sf::Vector2f player_position;
+	bool player_alive;
+	int player_score;
+
+public:
+	const sf::Vector2f left_most_position = sf::Vector2f(50.f, 0.f);
+	const sf::Vector2f right_most_position = sf::Vector2f(700.f,0.f);
+
+	const float player_movement_speed = 200.0f;
+	
+	PlayerModel();
+	~PlayerModel();
+
+	void initialize();
+
+	void reset();
+
+	sf::Vector2f getPlayerPosition();
+	void setPlayerPosition(sf::Vector2f position);
+
+	bool getPlayerAlive();
+	void setPlayerAlive(bool alive);
+
+
+
+
+};
---
+++ Space-Invaders/Source/PlayerModel.cpp
@@ -0,0 +1,32 @@
+#include "../Header/PlayerModel.h"
+
+PlayerModel::PlayerModel() {}
+PlayerModel::~PlayerModel() {}
+
+void PlayerModel::initialize() {}
+void PlayerModel::reset()
+{
+	player_alive = true;
+	player_position = initial_player_position;
+	player_score = 0;
+}
+
+sf::Vector2f PlayerModel::getPlayerPosition()
+{
+	return player_position;
+}
+
+void PlayerModel::setPlayerPosition(sf::Vector2f position)
+{
+	player_position = position;
+}
+
+bool PlayerModel::getPlayerAlive()
+{
+	return player_alive;
+}
+
+void PlayerModel::setPlayerAlive(bool alive)
+{
+	player_alive = alive;
+}
