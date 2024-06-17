7a915af10f325903206a522c3fcb074c45145345
prithbi0018 <prithbirajpanda@gmail.com>
2024-06-17 10:57:58 +0530

    Create Common Enemy Properties

---
+++ Space-Invaders/Header/Enemy/EnemConfig.h
@@ -0,0 +1,27 @@
+#pragma once
+#pragma once
+
+namespace Enemy
+{
+    enum class EnemyType
+    {
+        ZAPPER,
+        SUBZERO,
+        UFO,
+        THUNDER_SNAKE,
+    };
+
+    enum class EnemyState
+    {
+        PATROLLING,
+        ATTACK,
+        DEAD,
+    };
+
+    enum class MovementDirection
+    {
+        LEFT,
+        RIGHT,
+        DOWN,
+    };
+}
