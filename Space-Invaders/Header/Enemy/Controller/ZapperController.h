772eaea3dfea3eb37534aa38f12a195ca84c54da
prithbi0018 <prithbirajpanda@gmail.com>
2024-06-17 22:24:55 +0530

    Create Unique Enemy Functionality

--- Space-Invaders/Header/Enemy/Controller/ZapperController.h
+++ Space-Invaders/Header/Enemy/Controller/ZapperController.h
@@ -1,1 +1,25 @@
 #pragma once
+#include "../../header/Enemy/EnemyController.h"
+
+namespace Enemy
+{
+    namespace Controller
+    {
+        class ZapperController : public EnemyController
+        {
+        private:
+            float vertical_travel_distance = 100.f;
+
+            void move() override;
+            void moveLeft();
+            void moveRight();
+            void moveDown();
+
+        public:
+            ZapperController();
+            ~ZapperController();
+
+            void initialize() override;
+        };
+    }
+}
---
+++ Space-Invaders/Source/ZapperComtroller.cpp
@@ -0,0 +1,33 @@
+#include"../Header/Enemy/Controller/ZapperController.h"
+
+namespace Enemy
+{
+
+	namespace Controller
+	{
+		ZapperController::ZapperController() { }
+
+		ZapperController::~ZapperController() { }
+
+		void ZapperController::initialize()
+		{
+			EnemyController::initialize();;
+		}
+
+		void ZapperController::move()
+		{
+		}
+
+		void ZapperController::moveLeft()
+		{
+		}
+
+		void ZapperController::moveRight()
+		{
+		}
+
+		void ZapperController::moveDown()
+		{
+		}
+	}
+}
