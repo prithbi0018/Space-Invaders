3ea7fef947d0988554340f597e2f356cae656a7b
prithbi0018 <prithbirajpanda@gmail.com>
2024-06-20 11:36:56 +0530

    Creat Sound Service

---
+++ Space-Invaders/Source/SoundService.cpp
@@ -0,0 +1,27 @@
+#include "../../header/Sound/SoundService.h"
+
+
+namespace Sound
+{
+	using namespace Global;
+
+	void SoundService::initialize()
+	{
+	}
+
+	void SoundService::loadBackgroundMusicFromFile()
+	{
+	}
+
+	void SoundService::loadSoundFromFile()
+	{
+	}
+
+	void SoundService::playSound(SoundType soundType)
+	{
+	}
+
+	void SoundService::playBackgroundMusic()
+	{
+	}
+}
