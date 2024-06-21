44114eb933818fd3651004b9a872e7c4f0e0ca02
prithbi0018 <prithbirajpanda@gmail.com>
2024-06-21 22:05:49 +0530

    Create Projectile Interface

---
+++ Space-Invaders/Source/BulletService.cpp
@@ -0,0 +1,37 @@
+#include "../Header/Bullet/BulletService.h"
+
+namespace Element
+{
+    namespace Bullet
+    {
+        BulletService::BulletService()
+        {
+            // Constructor implementation (empty)
+        }
+
+        BulletService::~BulletService()
+        {
+            // Destructor implementation (empty)
+        }
+
+        void BulletService::initialize()
+        {
+            // Initialization code (empty)
+        }
+
+        void BulletService::update()
+        {
+            // Update code (empty)
+        }
+
+        void BulletService::render()
+        {
+            // Render code (empty)
+        }
+
+        void BulletService::destroy()
+        {
+            // Destroy code (empty)
+        }
+    }
+}
