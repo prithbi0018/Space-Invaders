73957aa1a9a01757e9ca73d55160e55b083345d6
prithbi0018 <prithbirajpanda@gmail.com>
2024-06-25 11:36:28 +0530

    spawend bullets

--- Space-Invaders/Source/BulletService.cpp
+++ Space-Invaders/Source/BulletService.cpp
@@ -1,37 +1,64 @@
-#include "../Header/Bullet/BulletService.h"
 
-namespace Element
+#include "../../Header/Bullet/BulletService.h"
+#include "../../Header/Bullet/BulletController.h"
+#include "../../Header/Bullet/BulletConfig.h"
+#include "../../Header/Bullet/Controllers/FrostBulletController.h"
+#include "../../Header/Bullet/Controllers/LaserBulletController.h"
+#include "../../Header/Bullet/Controllers/TorpedoController.h"
+
+namespace Bullet
 {
-    namespace Bullet
-    {
-        BulletService::BulletService()
-        {
-            // Constructor implementation (empty)
-        }
+	using namespace Controller;
+	using namespace Projectile;
 
-        BulletService::~BulletService()
-        {
-            // Destructor implementation (empty)
-        }
+	BulletService::BulletService() { }
 
-        void BulletService::initialize()
-        {
-            // Initialization code (empty)
-        }
+	BulletService::~BulletService() { destroy(); }
 
-        void BulletService::update()
-        {
-            // Update code (empty)
-        }
+	void BulletService::initialize() { }
 
-        void BulletService::render()
-        {
-            // Render code (empty)
-        }
+	void BulletService::update()
+	{
+		for (int i = 0; i < bullet_list.size(); i++) bullet_list[i]->update();
+	}
 
-        void BulletService::destroy()
-        {
-            // Destroy code (empty)
-        }
-    }
+	void BulletService::render()
+	{
+		for (int i = 0; i < bullet_list.size(); i++) bullet_list[i]->render();
+	}
+
+	BulletController* BulletService::createBullet(BulletType bullet_type)
+	{
+		switch (bullet_type)
+		{
+		case::Bullet::BulletType::LASER_BULLET:
+			return new LaserBulletController(Bullet::BulletType::LASER_BULLET);
+
+		case::Bullet::BulletType::FROST_BULLET:
+			return new FrostBulletController(Bullet::BulletType::FROST_BULLET);
+
+		case::Bullet::BulletType::TORPEDO:
+			return new TorpedoController(Bullet::BulletType::TORPEDO);
+		}
+	}
+
+	void BulletService::destroy()
+	{
+		for (int i = 0; i < bullet_list.size(); i++) delete (bullet_list[i]);
+	}
+
+	BulletController* BulletService::spawnBullet(BulletType bullet_type, sf::Vector2f position, MovementDirection direction)
+	{
+		BulletController* bullet_controller = createBullet(bullet_type);
+
+		bullet_controller->initialize(position, direction);
+		bullet_list.push_back(bullet_controller);
+		return bullet_controller;
+	}
+
+	void BulletService::destroyBullet(BulletController* bullet_controller)
+	{
+		bullet_list.erase(std::remove(bullet_list.begin(), bullet_list.end(), bullet_controller), bullet_list.end());
+		delete(bullet_controller);
+	}
 }
