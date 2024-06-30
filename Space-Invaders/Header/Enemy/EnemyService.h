d2de52ba9abfe803a9038091591d1f7aa6fc5295
prithbi0018 <prithbirajpanda@gmail.com>
2024-06-30 09:50:48 +0530

    Create Enemy And Powerup Collisions

--- Space-Invaders/Header/Enemy/EnemyService.h
+++ Space-Invaders/Header/Enemy/EnemyService.h
@@ -12,21 +12,25 @@ namespace Enemy
 		const float spawn_interval = 2.f;
 
 		std::vector<EnemyController*> enemy_list;
+		std::vector<EnemyController*> flagged_enemy_list;
 		float spawn_timer;
 
 		void updateSpawnTimer();
 		void processEnemySpawn();
 		EnemyType getRandomEnemyType();
 		EnemyController* createEnemy(EnemyType enemy_type);
+		void destroyFlaggedEnemies();
 		void destroy();
 
 	public:
 		EnemyService();
-		~EnemyService();
+		virtual ~EnemyService();
 
 		void initialize();
 		void update();
 		void render();
+
+		void reset();
 
 		EnemyController* spawnEnemy();
 		void destroyEnemy(EnemyController* enemy_controller);
--- Space-Invaders/Source/ElementService.cpp
+++ Space-Invaders/Source/ElementService.cpp
@@ -1,34 +1,125 @@
-#include "../Header/Element/ElementService.h"
+#include "../../header/Enemy/EnemyService.h"
+#include "../../header/Enemy/EnemyController.h"
+#include "../../header/Global/ServiceLocator.h"
+#include "../../header/Time/TimeService.h"
+#include "../../header/Enemy/EnemyConfig.h"
+#include "../../header/Enemy/Controller/ZapperController.h"
+#include "../../header/Enemy/Controller/ThunderSnakeController.h"
+#include "../../header/Enemy/Controller/SubzeroController.h"
+#include "../../header/Enemy/Controller/UFOController.h"
+#include "../../header/Collision/ICollider.h"
 
-namespace Element
+namespace Enemy
 {
-	ElementService::ElementService() { }
+	using namespace Global;
+	using namespace Time;
+	using namespace Controller;
+	using namespace Collision;
 
-	ElementService::~ElementService() { destroy(); }
+	EnemyService::EnemyService() { std::srand(static_cast<unsigned>(std::time(nullptr))); }
 
-	void ElementService::initialize()
+	EnemyService::~EnemyService() { destroy(); }
+
+	void EnemyService::initialize()
 	{
-		for (int i = 0; i < bunker_data_list.size(); i++)
-		{
-			Bunker::BunkerController* bunker_controller = new Bunker::BunkerController();
+		spawn_timer = spawn_interval;
+	}
 
-			bunker_controller->initialize(bunker_data_list[i]);
-			bunker_list.push_back(bunker_controller);
+	void EnemyService::update()
+	{
+		updateSpawnTimer();
+		processEnemySpawn();
+
+		for (EnemyController* enemy : enemy_list)
+			enemy->update();
+
+		destroyFlaggedEnemies();
+	}
+
+	void EnemyService::render()
+	{
+		for (EnemyController* enemy : enemy_list)
+			enemy->render();
+	}
+
+	void EnemyService::updateSpawnTimer()
+	{
+		spawn_timer += ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
+	}
+
+	void EnemyService::processEnemySpawn()
+	{
+		if (spawn_timer >= spawn_interval)
+		{
+			spawnEnemy();
+			spawn_timer = 0.0f;
 		}
 	}
 
-	void ElementService::update()
+	EnemyController* EnemyService::spawnEnemy()
 	{
-		for (int i = 0; i < bunker_list.size(); i++) bunker_list[i]->update();
+		EnemyController* enemy_controller = createEnemy(getRandomEnemyType());
+		enemy_controller->initialize();
+
+		ServiceLocator::getInstance()->getCollisionService()->addCollider(dynamic_cast<ICollider*>(enemy_controller));
+		enemy_list.push_back(enemy_controller);
+		return enemy_controller;
 	}
 
-	void ElementService::render()
+	EnemyType EnemyService::getRandomEnemyType()
 	{
-		for (int i = 0; i < bunker_list.size(); i++) bunker_list[i]->render();
+		int random_value = std::rand() % (static_cast<int>(Enemy::EnemyType::UFO) + 1);
+		return static_cast<Enemy::EnemyType>(random_value);
 	}
 
-	void ElementService::destroy()
+	EnemyController* EnemyService::createEnemy(EnemyType enemy_type)
 	{
-		for (int i = 0; i < bunker_list.size(); i++) delete(bunker_list[i]);
+		switch (enemy_type)
+		{
+		case::Enemy::EnemyType::ZAPPER:
+			return new ZapperController(Enemy::EnemyType::ZAPPER);
+
+		case::Enemy::EnemyType::THUNDER_SNAKE:
+			return new ThunderSnakeController(Enemy::EnemyType::THUNDER_SNAKE);
+
+		case::Enemy::EnemyType::SUBZERO:
+			return new SubzeroController(Enemy::EnemyType::SUBZERO);
+
+		case::Enemy::EnemyType::UFO:
+			return new UFOController(Enemy::EnemyType::UFO);
+		}
+	}
+
+	void EnemyService::destroyFlaggedEnemies()
+	{
+		for (int i = 0; i < flagged_enemy_list.size(); i++)
+		{
+			ServiceLocator::getInstance()->getCollisionService()->removeCollider(dynamic_cast<ICollider*>(flagged_enemy_list[i]));
+			delete (flagged_enemy_list[i]);
+		}
+		flagged_enemy_list.clear();
+	}
+
+	void EnemyService::destroyEnemy(EnemyController* enemy_controller)
+	{
+		dynamic_cast<ICollider*>(enemy_controller)->disableCollision();
+		flagged_enemy_list.push_back(enemy_controller);
+		enemy_list.erase(std::remove(enemy_list.begin(), enemy_list.end(), enemy_controller), enemy_list.end());
+	}
+
+	void EnemyService::destroy()
+	{
+		for (int i = 0; i < enemy_list.size(); i++)
+		{
+			ServiceLocator::getInstance()->getCollisionService()->removeCollider(dynamic_cast<ICollider*>(enemy_list[i]));
+			delete (enemy_list[i]);
+		}
+		enemy_list.clear();
+	}
+
+	void EnemyService::reset()
+	{
+		destroy();
+		spawn_timer = 0.0f;
 	}
 }
